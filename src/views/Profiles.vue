c372ddb20729a3710945ecc42d484f7393f91aa6
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-02-02 14:39:21 +0100

    added functionallity

--- src/views/Profiles.vue
+++ src/views/Profiles.vue
@@ -1,6 +1,71 @@
 <template>
-  <div>
-    <nav class="nav"><Logo /></nav>
+  <div class="profiles-body">
+    <nav class="nav"><Logo class="logo" /></nav>
+    <div class="wrapper">
+      <section class="section-main">
+        <div class="profiles-container" v-if="!isAddProfile">
+          <h1 class="profiles-container__heading">Who is watching?</h1>
+          <div class="cta-container">
+            <!--    <div class="card">
+            <font-awesome-icon :icon="['fas', 'female']" class="card__img" />
+            <p class="card__name">Christian</p>
+          </div>
+          <div class="card">
+            <font-awesome-icon :icon="['fas', 'child']" class="card__img" />
+            <p class="card__name">Christian</p>
+          </div>
+          <div class="card">
+            <font-awesome-icon :icon="['fas', 'smile']" class="card__img" />
+            <p class="card__name">Christian</p>
+          </div>
+          <div class="card">
+            <font-awesome-icon
+              :icon="['fas', 'user-astronaut']"
+              class="card__img"
+            />
+            <p class="card__name">Christian</p>
+          </div> -->
+            <div class="card" @click="goToAddProfile">
+              <font-awesome-icon
+                :icon="['far', 'plus-square']"
+                class="card__icon"
+              />
+              <p class="card__name">Add a profile</p>
+            </div>
+          </div>
+          <button class="profiles-container__btn">Manage profiles</button>
+        </div>
+        <div class="add-profile-container" v-else>
+          <h1 class="add-profile-container__heading">Add a profile</h1>
+          <p class=" add-profile-container__paragraph">
+            Add a profile for another person watching Netflix
+          </p>
+          <div class="add-profile">
+            <font-awesome-icon
+              :icon="['fas', 'female']"
+              class="add-profile__icon"
+            />
+            <input
+              type="text"
+              required
+              placeholder="Name"
+              class="add-profile__input"
+              v-model="profileName"
+            />
+          </div>
+          <div class="profile-cta">
+            <button class="profile-cta__btn " @click="addProfile">
+              Continue
+            </button>
+            <button class="profile-cta__btn profile-cta__btn--transparent">
+              Back
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+    <button @click="signout">dsdadasdas</button>
+    <h1>{{ profileData }}</h1>
   </div>
 </template>
 
@@ -11,20 +76,49 @@ import { mapActions } from 'vuex';
 import { mapGetters } from 'vuex';
 
 export default {
+  data() {
+    return {
+      isAddProfile: false,
+      profileName: null,
+      profileData: [],
+    };
+  },
+
   components: {
     Logo,
   },
   methods: {
-    /*   This is to set data in firebase
-    setUserData() {
+    goToAddProfile() {
+      this.isAddProfile = true;
+    },
+
+    addProfile() {
+      const currentUser = this.getCurrentUser;
+      const profiles = {
+        name: this.profileName,
+        icon: '',
+      };
       firebase
         .database()
-        .ref('users/' + this.currentUser.id)
-        .set({
-          email: this.currentUser.email,
-          profiles: [],
+        .ref('users/' + currentUser.id)
+        .push(profiles);
+
+      this.isAddProfile = false;
+    },
+
+    getProfiles() {
+      const uid = this.getCurrentUser.id;
+      firebase
+        .database()
+        .ref('users/' + uid)
+        .on('value', (snapshot) => {
+          snapshot.forEach((childSnapshot) => {
+            const childData = childSnapshot.val();
+            this.profileData.push(childData);
+          });
         });
-    }, */
+    },
+
     signout() {
       firebase.auth().signOut();
       this.$router.push({ path: '/signin' });
@@ -36,11 +130,12 @@ export default {
     ...mapGetters(['getCurrentUser']),
   },
   created() {
-    /*
-    this.setUserData(); */
     this.setCurrentUser();
+    this.getProfiles();
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '../sass/views/_profiles.scss';
+</style>
