ccfb9096d9f5e4ae596aa2ec7f34d78ddac1ea22
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-02-11 13:46:55 +0100

    removed old code

--- src/components/layout/nav/Navbar.vue
+++ src/components/layout/nav/Navbar.vue
@@ -12,29 +12,6 @@
         </ul>
 
         <NavDropdown v-if="mobileView" />
-        <!--   <dropdown-menu
-          v-model="show"
-          :right="right"
-          :hover="hover"
-          :interactive="interactive"
-          v-if="mobileView"
-        >
-          <button class="btn btn-primary dropdown-toggle">
-            Browse
-            <font-awesome-icon
-              :icon="['fas', 'angle-down']"
-              class="dropdown__icon"
-            />
-          </button>
-          <div slot="dropdown" class="dropdown__container">
-            <div class="dropdown__topbar"></div>
-            <a class="dropdown__item" href="#">Home</a>
-            <a class="dropdown__item" href="#">Series</a>
-            <a class="dropdown__item" href="#">Movies</a>
-            <a class="dropdown__item" href="#">Popular</a>
-            <a class="dropdown__item" href="#">My List</a>
-          </div>
-        </dropdown-menu> -->
 
         <div class="nav-right">
           <font-awesome-icon
