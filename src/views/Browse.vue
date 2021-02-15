5384f849ff00ac99a4a0a6be23e64a1af4495dad
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-02-15 14:14:47 +0100

    worked on showcase + video controls

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -2,8 +2,59 @@
   <div>
     <section class="showcase">
       <NavBar class="nav" />
+      <div class="movie-container">
+        <h1 class="movie-container__heading">Slasher</h1>
+        <p class="movie-container__paragraph">
+          Visions about Dantes inferno. Ledtrådar i en bottocsdo målning. Ett
+          virus som bara han kan stoppa, om han löser gåtan
+        </p>
+        <button class="movie-container__btn">
+          <font-awesome-icon
+            :icon="['fas', 'play']"
+            class="video-controls__icon video-controls__icon--play"
+          />Play
+        </button>
+        <button class="movie-container__btn movie-container__btn--info ">
+          <font-awesome-icon
+            :icon="['fas', 'info-circle']"
+            class="video-controls__icon video-controls__icon--info"
+          />
+          More info
+        </button>
+      </div>
+      <!-- Video controls -->
+      <div class="video-controls">
+        <font-awesome-icon
+          :icon="['fas', 'volume-mute']"
+          class="video-controls__icon"
+          @click="playSound"
+          v-if="isMuted"
+        />
+
+        <font-awesome-icon
+          :icon="['fas', 'volume-up']"
+          class="video-controls__icon"
+          @click="muteVideo"
+          v-if="!isMuted"
+        />
+
+        <transition name="fade">
+          <font-awesome-icon
+            :icon="['fa', 'history']"
+            class="video-controls__icon"
+            @click="replayVideo"
+            v-if="!isPlaying"
+        /></transition>
+        <div class="video-controls__rec-age">13+</div>
+      </div>
       <div class="video-container">
-        <video class="video" poster="poster.JPG" autoplay muted loop>
+        <video
+          class="video"
+          ref="video"
+          poster="poster.JPG"
+          autoplay
+          @ended="isVideoPaused"
+        >
           <source
             src="../assets/videos/pexels-vlado-pitbullgrif-6650121.mp4"
             type="video/mp4"
@@ -21,13 +72,38 @@ import { mapGetters } from 'vuex';
 import { mapActions } from 'vuex';
 
 export default {
+  data() {
+    return {
+      isMuted: false,
+      isPlaying: true,
+    };
+  },
   components: {
     NavBar,
   },
+
   methods: {
+    muteVideo() {
+      this.videoElement.muted = true;
+      this.isMuted = true;
+    },
+    playSound() {
+      this.videoElement.muted = false;
+      this.isMuted = false;
+    },
+    isVideoPaused() {
+      this.isPlaying = false;
+    },
+    replayVideo() {
+      this.isPlaying = true;
+      this.videoElement.play();
+    },
     ...mapActions(['fetchMovieData']),
   },
   computed: {
+    videoElement() {
+      return this.$refs.video;
+    },
     ...mapGetters(['getMovieData']),
   },
   created() {
@@ -37,23 +113,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.showcase {
-  position: relative;
-  right: 0;
-  width: 100%;
-  min-height: 100vh;
-}
-
-.nav {
-  position: absolute;
-  z-index: 5;
-}
-.video {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
+@import '../sass/views/_browse.scss';
 </style>
